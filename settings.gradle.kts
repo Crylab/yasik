693e10c32f95934747b906a472b01b3a4532261a
Danilo Pianini <danilo.pianini@unibo.it>
2024-01-23 15:50:08 +0100

    build: enforce conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.gradle.enterprise") version "3.15.1"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.0"
 }
 
 rootProject.name = "yasik"
@@ -11,4 +12,9 @@ gradleEnterprise {
         termsOfServiceAgree = "yes"
         publishOnFailure()
     }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks(overwriteExisting = true)
 }
