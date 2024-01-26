fe2e84c7479dd8ccfc70020086ea80d0fc512ae6
Ruslan Shaiakhmetov <ruslan@mbp-di-ruslan.guest.mdh.se>
2024-01-26 12:04:05 +0100

    yasik_interpreter.py style

--- yasik/yasik_interpreter.py
+++ yasik/yasik_interpreter.py
@@ -1,8 +1,8 @@
-from antlr4 import *
+from antlr4 import InputStream, CommonTokenStream
 from yasik.parser_generated.YasikLexer import YasikLexer
 from yasik.parser_generated.YasikParser import YasikParser
 from yasik.parser_generated.YasikListener import YasikListener
-from copy import copy
+
 
 def yasik_compiler(input_str: str,  param: list) -> str:
     input_stream = InputStream(input_str)
@@ -23,20 +23,20 @@ class YasikEvaluator(YasikListener):
         self.assignmentCycle = {}
 
     # Exit a parse tree produced by YasikParser#code.
-    def exitCode(self, ctx:YasikParser.CodeContext):
+    def exitCode(self, ctx: YasikParser.CodeContext):
         return ctx.getText()
 
     # Exit a parse tree produced by YasikParser#assignment.
-    def enterAssignment(self, ctx:YasikParser.AssignmentContext):
+    def enterAssignment(self, ctx: YasikParser.AssignmentContext):
         pass
 
     # Exit a parse tree produced by YasikParser#assignment.
-    def exitAssignment(self, ctx:YasikParser.AssignmentContext):
+    def exitAssignment(self, ctx: YasikParser.AssignmentContext):
         if ctx.getChildCount() == 3:
             left_in = ctx.children[0].getText()
             right = ctx.children[2].getText()
             if "self.xmlManager.xmlMetaReading" in left_in:
-                right+=")"
+                right += ")"
             symbol = ctx.children[1].getText()[0]
             if "self.xmlManager.xmlMetaReading" not in left_in:
                 left = left_in
@@ -57,19 +57,19 @@ class YasikEvaluator(YasikListener):
             ctx.children.append(element)
 
     # Exit a parse tree produced by YasikParser#arithmeticExpr.
-    def exitArithmeticExpr(self, ctx:YasikParser.ArithmeticExprContext):
+    def exitArithmeticExpr(self, ctx: YasikParser.ArithmeticExprContext):
         pass
 
     # Exit a parse tree produced by YasikParser#term.
-    def exitTerm(self, ctx:YasikParser.TermContext):
+    def exitTerm(self, ctx: YasikParser.TermContext):
         pass
 
     # Exit a parse tree produced by YasikParser#factor.
-    def exitFactor(self, ctx:YasikParser.FactorContext):
+    def exitFactor(self, ctx: YasikParser.FactorContext):
         pass
 
     # Exit a parse tree produced by YasikParser#yasik_slice.
-    def exitYasik_slice(self, ctx:YasikParser.Yasik_sliceContext):
+    def exitYasik_slice(self, ctx: YasikParser.Yasik_sliceContext):
 
         def sub(content):
             element = ctx.children[0]
@@ -77,24 +77,24 @@ class YasikEvaluator(YasikListener):
             ctx.children.clear()
             ctx.children.append(element)
 
-        if ctx.getChildCount() == 1: 
-            if ctx.children[0].getText() == ":": # :
+        if ctx.getChildCount() == 1:
+            if ctx.children[0].getText() == ":":
                 sub("0:MAXLIMITKEYWORD")
             else:  # N
                 sub(ctx.children[0].getText()+":"+str(int(ctx.children[0].getText())+1))
 
         if ctx.getChildCount() == 2:
-            if ctx.children[0].getText() == ":": # :N
+            if ctx.children[0].getText() == ":":
                 sub("0:"+ctx.children[1].getText())
-            else: # N:
+            else:
                 sub(ctx.children[0].getText()+":MAXLIMITKEYWORD")
-            
+
         if ctx.getChildCount() == 3: # N:N
             sub(ctx.children[0].getText()+":"+ctx.children[2].getText())
 
     # Exit a parse tree produced by YasikParser#functionCall.
-    def exitFunctionCall(self, ctx:YasikParser.FunctionCallContext):        
-    
+    def exitFunctionCall(self, ctx: YasikParser.FunctionCallContext):
+
         zero_string = "0:1"
 
         def sub(name, first_slice, second_slice, prefix=""):
@@ -109,25 +109,24 @@ class YasikEvaluator(YasikListener):
             ctx.children.clear()
             ctx.children.append(element)
 
-        if ctx.getChildCount() == 1: # param
+        if ctx.getChildCount() == 1:
             if ctx.children[0].getText() in self.param_list:
                 sub(ctx.children[0].getText(), zero_string, zero_string)
 
-        if ctx.getChildCount() == 3: # category.param
+        if ctx.getChildCount() == 3:
             sub(ctx.children[2].getText(), zero_string, zero_string, prefix=ctx.children[0].getText()+".")
 
-        if ctx.getChildCount() == 4: # param(Slice)
+        if ctx.getChildCount() == 4:
             sub(ctx.children[0].getText(), zero_string, ctx.children[2].getText())
-         
-        if ctx.getChildCount() == 6: 
-            if ctx.children[1].getText() == ".": # category.param(Slice)
-                sub(ctx.children[2].getText(), zero_string, ctx.children[4].getText(), prefix=ctx.children[0].getText()+".")
 
-            else: # param(Slice, Slice)
+        if ctx.getChildCount() == 6:
+            if ctx.children[1].getText() == ".":
+                sub(ctx.children[2].getText(), zero_string,
+                    ctx.children[4].getText(), prefix=ctx.children[0].getText() + ".")
+
+            else:
                 sub(ctx.children[0].getText(), ctx.children[2].getText(), ctx.children[4].getText())                
 
-        if ctx.getChildCount() == 8: # category.param(Slice, Slice)
-            sub(ctx.children[2].getText(), ctx.children[4].getText(), ctx.children[6].getText(), prefix=ctx.children[0].getText()+".") 
-    
-        
-            
+        if ctx.getChildCount() == 8:
+            sub(ctx.children[2].getText(), ctx.children[4].getText(),
+                ctx.children[6].getText(), prefix=ctx.children[0].getText() + ".")
