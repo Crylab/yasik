10d0421377b28f2405c1cf8e26fa48f4c772a3e8
Ruslan Shaiakhmetov <ruslan@mbp-di-ruslan.guest.mdh.se>
2024-01-26 11:59:42 +0100

    fix: yasik_exe.py style

--- yasik/yasik_exe.py
+++ yasik/yasik_exe.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python3
 
-from antlr4 import *
+from antlr4 import InputStream, CommonTokenStream
 from yasik_interpreter import YasikEvaluator
 from parser_generated.YasikLexer import YasikLexer
 from parser_generated.YasikParser import YasikParser
-from parser_generated.YasikListener import YasikListener
+
 
 def main():
     print("Hey!!!!")
-    for expression in ["lar", 
-                       "lar(5)", 
-                       "lar(5,6)", 
-                       "cat.lar", 
-                       "cat.lar(5)", 
-                       "cat.lar(5,6)", 
-                       "lar(5:16)", 
-                       "lar(5:16,7:18)", 
-                       "cat.lar(5:16)", 
-                       "cat.lar(5:16,7:18)", 
-                       "lar(5:)", 
-                       "lar(5:,7:)", 
-                       "cat.lar(5:)", 
-                       "cat.lar(5:,7:)", 
-                       "lar(:16)", 
-                       "lar(:16,:18)",                        
-                       "cat.lar(:16)", 
+    for expression in ["lar",
+                       "lar(5)",
+                       "lar(5,6)",
+                       "cat.lar",
+                       "cat.lar(5)",
+                       "cat.lar(5,6)",
+                       "lar(5:16)",
+                       "lar(5:16,7:18)",
+                       "cat.lar(5:16)",
+                       "cat.lar(5:16,7:18)",
+                       "lar(5:)",
+                       "lar(5:,7:)",
+                       "cat.lar(5:)",
+                       "cat.lar(5:,7:)",
+                       "lar(:16)",
+                       "lar(:16,:18)",
+                       "cat.lar(:16)",
                        "cat.lar(:16,:18)",
-                       "lar += cat.lar(5:)", 
+                       "lar += cat.lar(5:)",
                        "lar = cat.lar(5:)",
                        "lar = lar(1,2); lar(1,1) = lar(1,3)",
                        "var1 = lar(1,2); lar(1,1) = var1"]:
-    #expression = input("Enter an expression: ")
         input_stream = InputStream(expression)
-    #lexer = CalcLexer(input_stream)
         lexer = YasikLexer(input=input_stream)
         token_stream = CommonTokenStream(lexer)
         parser = YasikParser(token_stream)
@@ -40,10 +38,7 @@ def main():
         parser.addParseListener(evaluator)
         tree = parser.code()
         print(tree.toStringTree(recog=parser))
-    #calc_evaluator = CalcEvaluator()
-    #CalcEvaluator.stack = tree
-    #calc_evaluator.exitExpr(tree)
-    #val = calc_evaluator.getValue()
+
 
 if __name__ == '__main__':
     main()
