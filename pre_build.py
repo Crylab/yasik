8472cdba8744bc42537f757be279b471fcee8c85
Ruslan Shaiakhmetov <ruslan@mbp-di-ruslan.guest.mdh.se>
2024-01-26 10:45:34 +0100

    fix: style pre_build.py

--- pre_build.py
+++ pre_build.py
@@ -3,22 +3,22 @@ import subprocess
 import os
 import yaml
 import shutil
-from copy import copy
 command = "./gradlew" if os.name == "posix" else "gradlew.bat"
 subprocess.run([command, "generateGrammarSource"])
 
-## Tests generation
+# Tests generation
 print("Testing code generation initialization")
 # Make generation folder
-try:  
+try:
     os.mkdir("./tests/test_generated/")
-except OSError as error:  
-    print(error) 
+except OSError as error:
+    print(error)
 
 shutil.copy('./tests/test_sample.xml', './tests/test_generated/test_sample.xml')
 shutil.copy('./tests/fake_simulation.py', './tests/test_generated/fake_simulation.py')
 
 counter = 0
+
 
 def test_sequential_file_generation(name: str, dict: dict, preambula, test_code):
     if name:
@@ -34,11 +34,40 @@ def test_sequential_file_generation(name: str, dict: dict, preambula, test_code)
             counter += test_number+1
         print("Testing code file "+name+".py was sucessfully generated")
 
-preambula_seq = "#Automatically generated test file\nimport sys\nimport pytest\nsys.path.insert(1, '../')\nfrom yasik.yasik_interpreter import yasik_compiler\n\n"    
-test_code_seq = "def test_TEST_NUMBER():\n\tinput_code = \"INPUT_CODE\"\n\texpected_code = \"EXPECTED_CODE\"\n\tresult = yasik_compiler(input_code, {\"lar\": (210, 215)})\n\tif result != expected_code:\n\t\traise Exception(\"The input code:\\t\\t\"+input_code+\"\\nThe gotten result:\\t\\t\\t\"+result+\"\\nThe expected result:\\t\\t\\t\"+expected_code)\n\n\n"
-preambula_int = "#Automatically generated test file\nimport sys\nimport os\nimport pytest\nsys.path.insert(1, '../')\nfrom yasik.yasik_interpreter import yasik_compiler\nimport fake_simulation\n\n"
-test_code_int = "def test_TEST_NUMBER():\n\tinput_code = \"INPUT_CODE\"\n\texpected_code = \"EXPECTED_CODE\"\n\tscript_dir = os.path.dirname(os.path.abspath(__file__))\n\tfile_path = os.path.join(script_dir, \"test_sample.xml\")\n\tmatlab_code = yasik_compiler(input_code, fake_simulation.get_xml_param_dict(file_path))\n\tobj = fake_simulation.Test_Simulation(file_path)\n\tresult = obj.execute_meta_code(matlab_code)\n\tif str(result) != expected_code:\n\t\traise Exception(\"The input code:\\t\\t\"+input_code+\"\\nThe gotten result:\\t\\t\\t\"+str(result)+\"\\nThe expected result:\\t\\t\\t\"+expected_code)\n\n\n"
-test_code_wrt = "def test_TEST_NUMBER():\n\tinput_code = \"INPUT_CODE\"\n\texpected_code = \"EXPECTED_CODE\"\n\tscript_dir = os.path.dirname(os.path.abspath(__file__))\n\tfile_path = os.path.join(script_dir, \"test_sample.xml\")\n\tmatlab_code = yasik_compiler(input_code, fake_simulation.get_xml_param_dict(file_path))\n\tname=input_code.split(\"(\")\n\treading_code = yasik_compiler(name[0]+\"(:, :)\", fake_simulation.get_xml_param_dict(file_path))\n\tobj = fake_simulation.Test_Simulation(file_path)\n\tresult_before = obj.execute_meta_code(reading_code)\n\t_ = obj.execute_meta_code(matlab_code)\n\tobj.xmlManager.write_to_file()\n\tresult_after = obj.execute_meta_code(reading_code)\n\tobj.xmlManager.restore_backup()\n\tif str(result_after) == str(result_before):\n\t\traise Exception(\"The input code:\t\t\"+input_code+\" doesnt cause any changes in XML file\")\n\tif str(result_after) != expected_code:\n\t\traise Exception(\"The input code:\\t\\t\"+input_code+\"\\nThe gotten result:\\t\\t\\t\"+str(result_after)+\"\\nThe expected result:\\t\\t\\t\"+expected_code)\n\n\n"
+
+preambula_seq = """#Automatically generated test file\n
+                import sys\nimport pytest\nsys.path.insert(1, '../')\n
+                from yasik.yasik_interpreter import yasik_compiler\n\n"""
+test_code_seq = """def test_TEST_NUMBER():\n\tinput_code = \"INPUT_CODE\"\n\t
+                expected_code = \"EXPECTED_CODE\"\n\t
+                result = yasik_compiler(input_code, {\"lar\": (210, 215)})\n\t
+                if result != expected_code:\n\t\t
+                raise Exception(\"The input code:\\t\\t\"+input_code+\"\\nThe gotten result:\\t\\t\\t\"+result+\"\\n
+                The expected result:\\t\\t\\t\"+expected_code)\n\n\n"""
+preambula_int = """#Automatically generated test file\nimport sys\nimport os\nimport pytest\n
+                sys.path.insert(1, '../')\nfrom yasik.yasik_interpreter import yasik_compiler\n
+                import fake_simulation\n\n"""
+test_code_int = """def test_TEST_NUMBER():\n\tinput_code = \"INPUT_CODE\"\n\t
+                expected_code = \"EXPECTED_CODE\"\n\t
+                script_dir = os.path.dirname(os.path.abspath(__file__))\n\t
+                file_path = os.path.join(script_dir, \"test_sample.xml\")\n\t
+                matlab_code = yasik_compiler(input_code, fake_simulation.get_xml_param_dict(file_path))\n\t
+                obj = fake_simulation.Test_Simulation(file_path)\n\tresult = obj.execute_meta_code(matlab_code)\n\t
+                if str(result) != expected_code:\n\t\traise Exception(\"The input code:\\t\\t\"+input_code+\"\\n
+                The gotten result:\\t\\t\\t\"+str(result)+\"\\nThe expected result:\\t\\t\\t\"+expected_code)\n\n\n"""
+test_code_wrt = """def test_TEST_NUMBER():\n\tinput_code = \"INPUT_CODE\"\n\texpected_code = \"EXPECTED_CODE\"\n\t
+                script_dir = os.path.dirname(os.path.abspath(__file__))\n\t
+                file_path = os.path.join(script_dir, \"test_sample.xml\")\n\t
+                matlab_code = yasik_compiler(input_code, fake_simulation.get_xml_param_dict(file_path))\n\t
+                name=input_code.split(\"(\")\n\t
+                reading_code = yasik_compiler(name[0]+\"(:, :)\", fake_simulation.get_xml_param_dict(file_path))\n\t
+                obj = fake_simulation.Test_Simulation(file_path)\n\t
+                result_before = obj.execute_meta_code(reading_code)\n\t_ = obj.execute_meta_code(matlab_code)\n\t
+                obj.xmlManager.write_to_file()\n\tresult_after = obj.execute_meta_code(reading_code)\n\t
+                obj.xmlManager.restore_backup()\n\tif str(result_after) == str(result_before):\n\t\t
+                raise Exception(\"The input code:\t\t\"+input_code+\" doesnt cause any changes in XML file\")\n\t
+                if str(result_after) != expected_code:\n\t\traise Exception(\"The input code:\\t\\t\"+input_code+\"\\n
+                The gotten result:\\t\\t\\t\"+str(result_after)+\"\\nThe expected result:\\t\\t\\t\"+expected_code)\n\n\n"""
 
 with open("./tests/testing_data.yaml", "r") as stream:
     try:
